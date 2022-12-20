869c2610e4639383be6e408e2a7854a371b78d65
Archit Poddar <archit.poddar@gmail.com>
2022-12-20 21:26:10 +0530

    * Add style for the first n-number of paragraphs

--- XamarinHtmlWebview/XamarinHtmlWebview/Helpers/StringHelper.cs
+++ XamarinHtmlWebview/XamarinHtmlWebview/Helpers/StringHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using Xamarin.Forms;
 
 namespace XamarinHtmlWebview.Helpers
@@ -7,6 +8,16 @@ namespace XamarinHtmlWebview.Helpers
 	{
         public static string CustomizeHtmlData(string htmlString, string fontName = "SourceSansPro-Regular.ttf")
         {
+
+            var heading = htmlString.Split(new string[] { "</strong></p>" }, StringSplitOptions.None);
+
+            var title = heading[0];
+            var body = heading[1];
+
+            title = "<h2>" + title + "</strong></p></h2>";
+
+            var finalHtml = title + body;
+
             var fontPath = DependencyService.Get<IBaseUrl>().Get() + $"/Fonts/{fontName}";
 
             var html = $@"
@@ -24,9 +35,118 @@ namespace XamarinHtmlWebview.Helpers
                     </style>
                 </head>
                 <body>
-                {htmlString}
+                {finalHtml}
                 </body>
             </html>";
+
+            return html;
+        }
+
+
+        public static string FontFaceRules()
+        {
+            string fontFaceRules =
+                "@font-face {" +
+                "    font-family: 'SourceSansPro';" +
+                "    src: url('fonts/SourceSansPro-Regular.ttf');" +
+                "}" +
+                "@font-face {" +
+                "    font-family: 'SourceSansPro';" +
+                "    font-weight: bold;" +
+                "    src: url('fonts/SourceSansPro-Bold.ttf');" +
+                "}" +
+                "@font-face {" +
+                "    font-family: 'SourceSansPro';" +
+                "    font-style: italic;" +
+                "    src: url('fonts/SourceSansPro-Italic.ttf');" +
+                "}" +
+                "@font-face {" +
+                "    font-family: 'SourceSansPro';" +
+                "    font-weight: bold;" +
+                "    font-style: italic;" +
+                "    src: url('fonts/SourceSansPro-BoldItalic.ttf');" +
+                "}";
+
+
+
+            return fontFaceRules;
+        }
+
+        public static string WrapInHtmlWithBodyEvent(string htmlSnipet, string bodyEvent = "", string fontSize = "16", int noOfTitleParagraphs = 1)
+        {
+            //var heading = htmlSnipet.Split(new string[] { "</strong></p>" }, StringSplitOptions.None);
+
+            //var title = heading[0];
+            //var body = heading[1];
+
+            //title = @"<div class=""titleLabel"">" + title + "</strong></p></div>";
+
+            //htmlSnipet = title + body;
+
+            string paraTitleStyle = "";
+
+            var css = "{ font-size: 30pt; color: #FF0000; }";
+
+            if (noOfTitleParagraphs == 1)
+            {
+                paraTitleStyle = @"p:first-child" + css;
+            }
+            else if(noOfTitleParagraphs > 1)
+            {
+                paraTitleStyle = "p:first-child";
+
+                string additionalParaStyle = "p:first-child";
+
+                for (int i = 2; i <= noOfTitleParagraphs; i++)
+                {
+                    additionalParaStyle = $"{additionalParaStyle}+p";
+                    paraTitleStyle = paraTitleStyle + "," + additionalParaStyle;
+                }
+
+                paraTitleStyle = paraTitleStyle + css;
+            }
+
+            string fontFaceRules = FontFaceRules();
+            if (!string.IsNullOrEmpty(bodyEvent))
+                bodyEvent = $" {bodyEvent}";
+
+            string html =
+                "<!DOCTYPE html>" +
+                "<html>" +
+                "<head>" +
+                "    <meta charset='utf-8'>" +
+                "    <style>" +
+               $"        {fontFaceRules}" +
+                "        html { -webkit-text-size-adjust: none; }" +
+                "        body, table, h1, h2, h3, h4, h5 {" +
+                "            font-family: 'SourceSansPro';" +
+                "        }" +
+                "        body {" +
+               $"            font-size: {fontSize}pt;" +
+                "            margin: 0px;" +
+                "            padding: 0px;" +
+                "            -webkit-text-size-adjust: none;" +
+                "            color: #584F75;" +
+                "        }" +
+                "        .titleLabel {" +
+                $"            font-size: 18pt;" +
+                "            color: #00FF00;" +
+                "        }" +
+                $"{paraTitleStyle}"+
+                //"        p:first-child {" +
+                //$"            font-size: 30pt;" +
+                //"            color: #FF0000;" +
+                //"        }" +
+                //"        p:first-child + p + p {" +
+                //$"            font-size: 30pt;" +
+                //"            color: #FF0000;" +
+                //"        }" +
+                "    </style>" +
+                "</head>" +
+               $"<body{bodyEvent}>" +
+               $"    {htmlSnipet}" +
+                "</body>" +
+                "</html>";
 
             return html;
         }
--- XamarinHtmlWebview/XamarinHtmlWebview/MainPage.xaml.cs
+++ XamarinHtmlWebview/XamarinHtmlWebview/MainPage.xaml.cs
@@ -18,9 +18,12 @@ namespace XamarinHtmlWebview
             InitializeComponent();
 
             //var html = @"<html><head><style type=text/css> body{background-color: #636363;color: blue;font-size: 80px;}p{color: yellow;font-size: 60px;}</style></head><body><p><strong>This is a sample title</strong><p><p> This is a sample paragraph text.</body></html>";
-            var html = "<p><strong>This is a sample title</strong><p><p> This is a sample paragraph text.";
+            var html = @"<p><strong>Terms and conditions for BD™ Diabetes Care App</strong></p><p>Effective date: 4/1/2022</p><p >PLEASE READ THIS AGREEMENT (“AGREEMENT”) CAREFULLY BEFORE USING THE BD DIABETES CARE SOFTWARE APPLICATION (“APP”) OFFERED BY  EMBECTA (REFERRED TO AS “ EMBECTA”, “WE”, “US”, or “OUR” AS APPLICABLE) <strong>FOR USE IN THE USA ONLY</strong>. BY DOWNLOADING THE APP OR USING THE APP IN ANY MANNER, YOU AGREE THAT YOU HAVE READ AND AGREE TO BE BOUND BY AND BE A PARTY TO THE TERMS AND CONDITIONS OF THIS AGREEMENT TO THE EXCLUSION OF ALL OTHER TERMS. IF THE TERMS OF THIS AGREEMENT ARE CONSIDERED AN OFFER, ACCEPTANCE IS EXPRESSLY LIMITED TO SUCH TERMS. IF YOU DO NOT UNCONDITIONALLY AGREE TO ALL THE TERMS AND CONDITIONS OF THE AGREEMENT, YOU HAVE NO RIGHT TO USE THE APP.</p>";
 
-            HtmlString = StringHelper.CustomizeHtmlData(html);
+            //HtmlString = html;
+
+            //HtmlString = StringHelper.CustomizeHtmlData(html);
+            HtmlString = StringHelper.WrapInHtmlWithBodyEvent(html, noOfTitleParagraphs: 3);
 
             BindingContext = this;
         }
